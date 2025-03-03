71847194ae419c4b7c2f6286770dc53f7948a2e7
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-03-04 04:19:10 +0900

    Makefile improvement

--- compiler_opt/sl/unrolling/rts/unrolling_profiler_test.cpp
+++ compiler_opt/sl/unrolling/rts/unrolling_profiler_test.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-
 extern "C" void __mlgo_unrolled_loop_begin(void);
 extern "C" void __mlgo_unrolled_loop_end(void);
 
-__attribute__((optnone))
-void doWork() {
-  int a;
+__attribute__((optnone)) void doWork() {
+  [[maybe_unused]] int a = 0;
   for (int i = 0; i < 100000; ++i) {
     a += 5;
   }
